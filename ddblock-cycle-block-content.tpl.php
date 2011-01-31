73b6beb89029d23625abad94d4fd47b440daf7fa
Philip Blaauw <ppblaauw@gmail.com>
2011-01-31 10:15:33 +0000

    Initial commit of Dynamic display block module Drupal 7.

--- ddblock-cycle-block-content.tpl.php
+++ ddblock-cycle-block-content.tpl.php
@@ -1,42 +1,38 @@
 <?php
-// $Id$ 
+// $Id$
 
-/**
+/*
  * @file
  * Default theme implementation to display a dynamic display blocks from a dynamic display block instance.
  *
  * Available variables:
- * - $ddblock_origin: Original module of the block.
- * - $ddblock_delta: Block number of the block.
- * - $ddblock_pager: Pager type to add the dynamic display block.
- * - $ddblock_pager_height: Pager container height.
- * - $ddblock_pager_width: Pager container width.
- * - $ddblock_imgcache_pager_item: Image cache preset name for the pager item.
- * - $ddblock_content: themed content.
+ * - $origin: Original module of the block.
+ * - $delta: Block number of the block.
+ * - $pager: Pager type to add the dynamic display block.
+ * - $pager_height: Pager container height.
+ * - $pager_width: Pager container width.
+ * - $imgcache_pager_item: Image cache preset name for the pager item.
+ * - $content: themed content.
+ *
+ * notes: don't change the ID names, they are used by the jQuery script.
  */
+ $settings = $ddblock_cycle_slider_settings;
+
 ?>
 
-
 <!-- block content. -->
-<div id="ddblock-<?php print $ddblock_delta; ?>" class="ddblock-contents clear-block" >
- <div class="ddblock-content clear-block">
-  <?php if ($ddblock_content): ?>
-   <?php if (($ddblock_pager == 'number-pager')): ?>
+<div id="ddblock-<?php print $settings['delta']; ?>" class="ddblock-contents clearfix" style="visibility:hidden">
+
+ <div class="ddblock-content clearfix">
+
+  <?php if ($content): ?>
+
+   <?php if (($settings['pager'] == 'number-pager')): ?>
     <!-- number pager. -->
-    <div id="ddblock-<?php print $ddblock_pager ."-". $ddblock_delta ?>" class="ddblock-<?php print $ddblock_pager ?> ddblock-pager clear-block" style="height: <?php print $ddblock_pager_height ?>px; width:<?php print $ddblock_pager_width ?>px;">
+    <div id="ddblock-<?php print $settings['pager'] ."-". $settings['delta'] ?>" class="ddblock-<?php print $settings['pager'] ?> ddblock-pager clearfix" style="height: <?php print $settings['pager_height']; ?>px; width:<?php print $settings['pager_width']; ?>px;">
      <?php $item_counter=1; ?>
-     <?php if (($ddblock_pager2 == 1)): ?>
-	  <ul>
-       <li>
-        <a class="prev" href="#"><?php print $ddblock_prev?></a>
-       </li>
-       <li>
-        <a class="next" href="#"><?php print $ddblock_next ?></a>
-       </li>
-	  </ul>
-     <?php endif; ?> 
      <ul>
-      <?php foreach ($ddblock_content as $item): ?>
+      <?php foreach ($content['slide_image'] as $item): ?>
        <li class="number-pager-item">
         <a href="#" class="pager-link" title="click to navigate to topic">
          <?php print $item_counter; ?>
@@ -46,74 +42,67 @@
       <?php endforeach; ?>
      </ul>
     </div>
-   <?php endif; ?>  
+   <?php endif; ?>
 
-   <?php if (($ddblock_pager == 'image-pager')): ?>
+   <?php if (($settings['pager'] == 'image-pager')): ?>
+
     <!-- image pager. -->
-    <div id="ddblock-<?php print $ddblock_pager ."-". $ddblock_delta ?>" class="ddblock-<?php print $ddblock_pager ?> ddblock-pager clear-block" style="height: <?php print $ddblock_pager_height ?>px; width:<?php print $ddblock_pager_width ?>px;">
-     <?php if (($ddblock_pager2 == 1)): ?>
-      <ul>
-	   <li>
-        <a class="prev" href="#"><?php print $ddblock_prev?></a>
-       </li>
+    <ul id="ddblock-<?php print $settings['pager'] ."-". $settings['delta'] ?>" class="ddblock-<?php print $settings['pager'] ?> ddblock-pager clearfix" style="height: <?php print $settings['pager_height'] ?>px; width:<?php print $settings['pager_width'] ?>px;">
+      <?php foreach ($content['pager_image'] as $image_file): ?>
        <li>
-        <a class="next" href="#"><?php print $ddblock_next?></a>
+        <a href="#" title="click to navigate to topic"><?php print $image_file; ?></a>
        </li>
-	  </ul>  
-     <?php endif; ?> 
-     <ul>
-      <?php if($ddblock_imgcache_pager_item != '<none>'):?>
-       <?php foreach ($ddblock_content as $image_file): ?>
-        <li class="image-pager-item">
-         <a href="#" class="pager-link" title="click to navigate to topic">
-          <?php print theme('imagecache', $ddblock_imgcache_pager_item, $image_file); ?>
-         </a>
-        </li>
-       <?php endforeach; ?>    
-      <?php else :?>
-       <?php foreach ($ddblock_content as $image_file): ?>
-        <li class="image-pager-item">
-         <a href="#" class="pager-link" title="click to navigate to topic"><img src="<?php print base_path() . $image_file; ?>" alt="" width="55" height="55" /></a>
-        </li>
-       <?php endforeach; ?> 
-      <?php endif;?>
-     </ul>
+      <?php endforeach; ?>
+    </ul>
+   <?php endif; ?>
+
+   <?php if ($settings['pager'] == 'prev-next-pager'): ?>
+    <!-- prev next pager. -->
+    <div id="ddblock-<?php print $settings['pager'] ."-". $settings['delta'] ?>" class="ddblock-<?php print $settings['pager'] ?> ddblock-pager clearfix" style="height: <?php print $settings['pager_height'] ?>px; width:<?php print $settings['pager_width'] ?>px;">
+     <a id="prev" class="prev" href="#">Prev</a>
+     <a id="next" class="next" href="#">Next</a>
     </div>
    <?php endif; ?>
 
-   <?php if ($ddblock_pager2 == 1 && $ddblock_pager == 'none'): ?>  
-    <!-- prev next pager. -->
-    <div id="ddblock-<?php print $ddblock_pager ."-". $ddblock_delta ?>" class="ddblock-<?php print $ddblock_pager ?> ddblock-pager clear-block" style="height: <?php print $ddblock_pager_height ?>px; width:<?php print $ddblock_pager_width ?>px;">
-     <a class="prev" href="#"><?php print $ddblock_prev?></a>
-     <a class="next" href="#"><?php print $ddblock_next?></a>
+   <?php if ($settings['output_type'] == 'images') : ?>
+    <div class="ddblock-container clearfix">
+     <div class="slider">
+      <?php foreach ($content['slide_image'] as $image_file): ?>
+       <div class="slide"><?php print $image_file; ?></div>
+      <?php endforeach; ?>
+     <div>
+     <!-- prev/next pager on slide -->
+     <?php if ($settings['pager2'] == 1): ?>
+      <div class="pager-slide prev-container prev-container-<?php print $settings['pager_position'] ?>">
+       <a class="prev" href="#"><?php print $settings['pager2_slide_prev']?></a>
+      </div>
+      <div class="pager-slide next-container next-container-<?php print $settings['pager_position'] ?>">
+       <a class="next" href="#"><?php print $settings['pager2_slide_next'] ?></a>
+      </div>
+     <?php endif; ?>
     </div>
-   <?php endif; ?>  
-  
-   <?php if ($ddblock_output_type == 'images') : ?>
-    <div class="ddblock-container visibility:hidden"> 
-     <?php if($ddblock_imgcache_slide != '<none>'):?>
-      <?php foreach ($ddblock_content as $image_file): ?>
-       <?php print theme('imagecache', $ddblock_imgcache_slide, $image_file); ?>
-      <?php endforeach; ?>    
-     <?php else :?>
-      <?php foreach ($ddblock_content as $image_file): ?>
-       <img src="<?php print base_path() . $image_file; ?>" alt=""/>
-      <?php endforeach; ?>    
-     <?php endif;?>
-     </div>
    <?php endif; ?>
-  
-   <?php if ($ddblock_output_type == 'content_array') : ?>
-    <div class="ddblock-container"> 
-     <?php foreach ($ddblock_content as $item): ?>
+
+   <?php if ($settings['output_type'] == 'content_array') : ?>
+    <div class="ddblock-container">
+     <?php foreach ($content as $item): ?>
       <?php print($item); ?>
      <?php endforeach; ?>
+     <!-- prev/next pager on slide -->
+     <?php if ($settings['pager2'] == 1): ?>
+      <div class="pager-slide prev-container prev-container-<?php print $settings['pager_position'] ?>">
+       <a class="prev" href="#"><?php print $settings['pager2_slide_prev']?></a>
+      </div>
+      <div class="pager-slide next-container next-container-<?php print $settings['pager_position'] ?>">
+       <a class="next" href="#"><?php print $settings['pager2_slide_next'] ?></a>
+      </div>
+     <?php endif; ?>
     </div>
    <?php endif; ?>
 
-   <?php if ($ddblock_output_type == 'view_content') : ?>
-    <div class="ddblock-container"> 
-     <?php print($ddblock_content); ?>
+   <?php if ($settings['output_type'] == 'view_content') : ?>
+    <div class="ddblock-container">
+     <?php print($content); ?>
     </div>
    <?php endif; ?>
   <?php endif; ?>
